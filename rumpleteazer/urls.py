16b429b293271b00bdb4f0bc24d26faea97d53d7
xiongchiamiov <xiong.chiamiov@gmail.com>
2009-08-28 17:09:47 -0700

    spaces to tabs, remove header from copy+paste, add utf-8 declaration

--- rumpleteazer/urls.py
+++ rumpleteazer/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
@@ -5,13 +7,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^rumpleteazer/', include('rumpleteazer.foo.urls')),
+	# Example:
+	# (r'^rumpleteazer/', include('rumpleteazer.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+	# to INSTALLED_APPS to enable admin documentation:
+	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	(r'^admin/', include(admin.site.urls)),
 )
